c588d6490f19257d3ca239d937968a0a23656f51
AricBa <bajingyun@163.com>
2015-12-02 17:03:24 +0800

    0.01

--- www/js/routes/po/po.controller.js
+++ www/js/routes/po/po.controller.js
@@ -1,6 +1,3 @@
-/**
- * Created by C5226508 on 11/2/2015.
- */
 (function () {
   'use strict';
   angular
--- www/js/routes/user/user.route.js
+++ www/js/routes/user/user.route.js
@@ -1,6 +1,3 @@
-/**
- * Created by C5226508 on 11/5/2015.
- */
 (function () {
   'use strict';
 
